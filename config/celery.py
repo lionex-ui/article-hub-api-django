0c9ed2e3a044e8dc4512cf8665bcf6a92a3a28de
lionex-ui <eugenemescaline@gmail.com>
2025-11-16 22:21:22 +0700

    some commit

--- config/celery.py
+++ config/celery.py
@@ -1,10 +1,19 @@
 import os
+import ssl
 
 from celery import Celery
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
 
-app = Celery("articlehub")
+app = Celery(
+    "articlehub",
+    broker_use_ssl={
+        "ssl_cert_reqs": ssl.CERT_NONE,
+    },
+    redis_backend_use_ssl={
+        "ssl_cert_reqs": ssl.CERT_NONE,
+    }
+)
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,26 +1,7 @@
 #!/bin/sh
 
-echo "Waiting for PostgreSQL to be ready..."
-python << END
-import sys
-import time
-from django.db import connection
-
-for i in range(30):
-    try:
-        connection.ensure_connection()
-        print("PostgreSQL is ready!")
-        break
-    except Exception:
-        print(f"Waiting for PostgreSQL... ({i+1}/30)")
-        time.sleep(2)
-else:
-    print("PostgreSQL not available after 60 seconds")
-    sys.exit(1)
-END
-
 echo "Applying database migrations..."
-python manage.py migrate --noinput
+/venv/bin/python manage.py migrate --noinput
 
 echo "Starting application..."
-exec "$@"
+exec /venv/bin/$@
